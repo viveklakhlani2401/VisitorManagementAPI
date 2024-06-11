26583fdfb62980df276ccb2eed3639147f50bf77
Vivek Lakhlani <104050764+viveklakhlani2401@users.noreply.github.com>
2024-06-11 14:23:29 +0530

    Update settings.py

--- QIT/settings.py
+++ QIT/settings.py
@@ -186,6 +186,6 @@ EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSCODE')
 
 from datetime import timedelta
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
